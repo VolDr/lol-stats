04a72253de8aa9b08ddb15723fd698ca2d766874
VolDr <voldr4396@yandex.ru>
2026-07-17 16:14:25 +0300

    export betting api

--- src/lol_stats/__init__.py
+++ src/lol_stats/__init__.py
@@ -1,17 +1,29 @@
 """League of Legends statistics toolkit."""
 
-from .analytics import MissingTeamError, WinProbabilities, matchup_probability, win_rate
+from .analytics import (
+    MissingTeamError,
+    WinProbabilities,
+    kill_score_probability,
+    matchup_probability,
+    win_rate,
+)
+from .betting import BettingReport, BettingStrategy, evaluate_betting
 from .config import Settings
 from .db import MatchRepository
-from .evaluation import EvaluationReport, evaluate_temporally
+from .evaluation import EloModel, EvaluationReport, evaluate_temporally
 
 __all__ = [
+    "BettingReport",
+    "BettingStrategy",
+    "EloModel",
     "EvaluationReport",
     "MatchRepository",
     "MissingTeamError",
     "Settings",
     "WinProbabilities",
+    "evaluate_betting",
     "evaluate_temporally",
+    "kill_score_probability",
     "matchup_probability",
     "win_rate",
 ]
